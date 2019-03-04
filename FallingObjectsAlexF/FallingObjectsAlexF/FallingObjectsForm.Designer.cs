61054641c6038dcebcbab634a4b44cef6a661119
AForgeron <Alexander.forgeron@ocsbstudent.ca>
2019-03-04 12:47:56 -0500

    Initial commit

---
+++ FallingObjectsAlexF/FallingObjectsAlexF/FallingObjectsForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace FallingObjectsAlexF
+{
+    partial class frmFallingObjects
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(265, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(49, 142);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(260, 20);
+            this.txtInput.TabIndex = 2;
+            // 
+            // frmFallingObjects
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 402);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "frmFallingObjects";
+            this.Text = "Falling Objects Alex Forgeron";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
+
