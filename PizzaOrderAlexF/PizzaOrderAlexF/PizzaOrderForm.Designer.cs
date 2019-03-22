0dc40eac61d847029e3787906b018364fcd76ae8
AForgeron <Alexander.forgeron@ocsbstudent.ca>
2019-03-22 13:14:00 -0400

    e

--- PizzaOrderAlexF/PizzaOrderAlexF/PizzaOrderForm.Designer.cs
+++ PizzaOrderAlexF/PizzaOrderAlexF/PizzaOrderForm.Designer.cs
@@ -28,20 +28,180 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.nudSize = new System.Windows.Forms.NumericUpDown();
+            this.nudToppings = new System.Windows.Forms.NumericUpDown();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudToppings)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(224, 305);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(61, 215);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(174, 342);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(61, 183);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(61, 137);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(62, 105);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "label8";
+            // 
+            // nudSize
+            // 
+            this.nudSize.Location = new System.Drawing.Point(390, 137);
+            this.nudSize.Maximum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.nudSize.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudSize.Name = "nudSize";
+            this.nudSize.Size = new System.Drawing.Size(120, 20);
+            this.nudSize.TabIndex = 9;
+            this.nudSize.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // nudToppings
+            // 
+            this.nudToppings.Location = new System.Drawing.Point(390, 259);
+            this.nudToppings.Maximum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.nudToppings.Name = "nudToppings";
+            this.nudToppings.Size = new System.Drawing.Size(120, 20);
+            this.nudToppings.TabIndex = 10;
+            this.nudToppings.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(416, 342);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 11;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
             // 
             // frmPizzaOrder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(610, 444);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.nudToppings);
+            this.Controls.Add(this.nudSize);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "frmPizzaOrder";
             this.Text = "Pizza Order Alexander Forgeron";
+            ((System.ComponentModel.ISupportInitialize)(this.nudSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudToppings)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown nudSize;
+        private System.Windows.Forms.NumericUpDown nudToppings;
+        private System.Windows.Forms.Button btnCalculate;
     }
 }
 
--- PizzaOrderAlexF/PizzaOrderAlexF/PizzaOrderForm.cs
+++ PizzaOrderAlexF/PizzaOrderAlexF/PizzaOrderForm.cs
@@ -16,5 +16,62 @@ namespace PizzaOrderAlexF
         {
             InitializeComponent();
         }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            {
+                //Local Variables
+                double pizzaCost, toppingsCost, friesCost, sodaCost, subtotal, hst, total;
+                const double HST = 0.13;
+
+                //Pizza Cost
+                if (nudSize.Value == 1)
+                {
+                    pizzaCost = 7.99;
+                }
+                else
+                {
+                    if (nudSize.Value == 2)
+                    {
+                        pizzaCost = 9.99;
+                    }
+                    else
+                    {
+                        pizzaCost = 12.99;
+                    }
+                }
+
+                //Toppings Cost
+                if (nudToppings.Value == 1)
+                {
+                    toppingsCost = 0.75;
+                }
+                else
+                {
+                    if (nudToppings.Value == 2)
+                    {
+                        toppingsCost = 1.35;
+                    }
+                    else
+                    {
+                        if (nudToppings.Value == 3)
+                        {
+                            toppingsCost = 2.15;
+                        }
+                        else
+                        {
+                            if (nudToppings.Value == 4)
+                            {
+                                toppingsCost = 2.75;
+                            }
+                            else
+                            {
+                                toppingsCost = 3.35;
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
